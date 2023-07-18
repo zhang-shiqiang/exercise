ba6cd057fb63c8c718855ebbba9407769333e495
张世强 <zx944532395@sina.com>
2023-07-18 16:19:48 +0800

    feat：题目开发提交
    1、react-component-add 实现
    2、react-fix-add bug 解决

--- 4.react-fix-add/src/App.tsx
+++ 4.react-fix-add/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState, useCallback } from 'react';
-import './index.css';
+import React, { useRef, useState, useCallback } from "react";
+import "./index.css";
 
 async function increaseRemote(a: number) {
   await new Promise((resolve) => setTimeout(resolve, Math.random() * 1e3));
@@ -12,29 +12,24 @@ async function increaseRemote(a: number) {
  * 2. 由于是异步相加，自增过程中需要将 button 置为 disabled 状态，不可响应点击；
  *
  * 请找出下面代码的实现问题并改正。
+ * 1、useRef可以改变状态、但是它不会触发组件的重新渲染。可以使用 useState
+ * 2、调用两次实际上也不会执行两次自增、可以通过传参来解决点1的时候传1，点2 的时候传2
+ * 3、在已知increaseRemote 中每次都会 + 1的时候、所以在赋值时固定-掉咯1
  */
 function App() {
   const [count, setCount] = useState(0);
-  const loading = useRef(false);
+  const [disabled, setDisabled] = useState(false);
 
-  const increase = useCallback(async () => {
-    if (loading.current) {
-      return;
-    }
-    loading.current = true;
-    const data = await increaseRemote(count);
-    setCount(data);
-    loading.current = false;
-  }, [count]);
-
-  const handleClick = (num) => {
-    if (num === 1) {
-      increase();
-    } else if (num === 2) {
-      increase();
-      increase();
-    }
-  };
+  /**
+   * 自增函数
+   * num 自增数字 1/2
+   */
+  const increase = useCallback(async (num: number) => {
+    setDisabled(true);
+    const data = await increaseRemote(num);
+    setCount((prevCount: number) => prevCount + (data - 1));
+    setDisabled(false);
+  }, []);
 
   return (
     <div className="App">
@@ -42,20 +37,10 @@ function App() {
         <div>请按照题目要求，修正以下程序</div>
       </header>
       <section className="App-content">
-        <button
-          disabled={loading.current}
-          onClick={() => {
-            handleClick(1);
-          }}
-        >
+        <button disabled={disabled} onClick={() => increase(1)}>
           +1
         </button>
-        <button
-          disabled={loading.current}
-          onClick={() => {
-            handleClick(2);
-          }}
-        >
+        <button disabled={disabled} onClick={() => increase(2)}>
           +2
         </button>
         <p>数值：{count}</p>
